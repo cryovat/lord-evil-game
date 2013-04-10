45de3209c0e12f934375070ca84dd4077f742c5a
Jarl Erik Schmidt <github@jarlerik.com>
2013-04-10 02:41:32 +0200

    Submodule instead of git clone for frost

--- configure.sh
+++ configure.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# If the frost repo hasn't been cloned yet
-if [ ! -d "external/ldoc" ]; then
-    # Clone frost to get the framework bits and pieces
-    git clone https://github.com/cryovat/frost.git external/frost
-
-    # Make sure the frost repo pulls down externals and such
-    pushd external/frost
-    sh configure.sh
-    popd
-fi
-
 # If the frost fw folder hasn't been symlinked into
 # the src folder, do so. Love ignores symlinks, but ldoc
 # will follow them
@@ -18,7 +7,6 @@ if [ ! -d "src/fw" ]; then
     ln -s $(pwd)/external/frost/src/fw $(pwd)/src/fw
 fi
 
-# Steal the docmon.sh script from frost but change the ldoc path
 if [ ! -f "docmon.sh" ]; then
     sed -e "s/external/external\/frost\/external/" external/frost/docmon.sh > docmon.sh
     chmod +x docmon.sh
